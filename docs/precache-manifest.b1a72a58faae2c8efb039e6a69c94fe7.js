c950e1c27727e6230357b5c9f7c09122bf408d3e
GrafSoul <grafsoul@gmail.com>
2020-08-12 17:49:48 +0500

    feat: Added docs folder for public project.

---
+++ docs/precache-manifest.b1a72a58faae2c8efb039e6a69c94fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fde1b6d9c781fb8bc54e29865e43c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9279dc77b9bfd38210cd",
+    "url": "/static/css/main.092cb850.chunk.css"
+  },
+  {
+    "revision": "97a434d4a2faf082d149",
+    "url": "/static/js/2.5dd23241.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5dd23241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9279dc77b9bfd38210cd",
+    "url": "/static/js/main.987e080d.chunk.js"
+  },
+  {
+    "revision": "53b52728302e6317a1b1",
+    "url": "/static/js/runtime-main.61978ac7.js"
+  },
+  {
+    "revision": "3cb81606106b15cf59e02fca13adadee",
+    "url": "/static/media/logo.3cb81606.png"
+  },
+  {
+    "revision": "18cfe039ba49d1db731b3b35c80970cc",
+    "url": "/static/media/signal.18cfe039.mp3"
+  }
+]);
